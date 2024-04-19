24b0dd2455be06dce285b13d1bdc13a2f48c976e
Balou9 <emmanuel.owosekun@gmail.com>
2024-04-19 23:54:12 +0200

    fix: stores services by category

--- docs/index.js
+++ docs/index.js
@@ -16,37 +16,46 @@ function createFCSObjFromDocs(filePath) {
 
       for (const line of data.split('\n')) {
         if (line.startsWith('# ')) {
-          firstLevelSection = line.toLowerCase().replace(/^(.){2}/,'').replace(/\W/g, '_').trim();
+          firstLevelSection = line.toLowerCase().trim().replace(/^(.){2}/,'').replace(/\W/g, '_');
           sections[firstLevelSection] = sections[firstLevelSection] || {}; 
         } 
         if (line.startsWith('## ')) {
-          secondLevelSection = line.toLowerCase().replace(/^(.){3}/,'').replace(/\W/g, '_').trim(1)
+          secondLevelSection = line.toLowerCase().trim().replace(/^(.){3}/,'').replace(/\W/g, '_')
           sections[firstLevelSection][secondLevelSection] = sections[firstLevelSection][secondLevelSection] || {};
         } 
         if (line.startsWith('### ')) {
-          thirdLevelSection = line.toLowerCase().replace(/^(.){4}/,'').replace(/\W/g, '_').trim()
+          thirdLevelSection = line.toLowerCase().trim().replace(/^(.){4}/,'').replace(/\W/g, '_')
           sections[firstLevelSection][secondLevelSection][thirdLevelSection] = sections[firstLevelSection][secondLevelSection][thirdLevelSection] || {};
         } 
         if (line.startsWith('#### ')) {
-          fourthLevelSection = line.toLowerCase().replace(/^(.){5}/,'').replace(/\W/g, '_').trim()
+          fourthLevelSection = line.toLowerCase().trim().replace(/^(.){5}/,'').replace(/\W/g, '_')
            sections[firstLevelSection][secondLevelSection][thirdLevelSection][fourthLevelSection] = sections[firstLevelSection][secondLevelSection][thirdLevelSection][fourthLevelSection] || {}
         } 
         if (line.startsWith('[')) {
           let closingBracketIndex = line.indexOf(']');
           let serviceKey = line.substring(0, closingBracketIndex + 1);
           let serviceValue = line.substring(closingBracketIndex + 1); 
-          console.log(serviceKey);
-          console.log(serviceValue);  
-          console.log({
-            "key": serviceKey,
-            "value": serviceValue,
-            "line": line,
-            "firstLevelSection": firstLevelSection, 
-            "secondLevelSection": secondLevelSection, 
-            "thirdLevelSection": thirdLevelSection, 
-            "fourthLevelSection": fourthLevelSection
-          })
-          console.log(sections)
+          if (sections[firstLevelSection]?.[secondLevelSection]?.[thirdLevelSection]?.[fourthLevelSection]) {
+            sections[firstLevelSection][secondLevelSection][thirdLevelSection][fourthLevelSection][serviceKey] = serviceValue
+          } else if (sections[firstLevelSection]?.[secondLevelSection]?.[thirdLevelSection]) {
+            sections[firstLevelSection][secondLevelSection][thirdLevelSection][serviceKey] = serviceValue
+          } else if (sections[firstLevelSection]?.[secondLevelSection]) {
+            sections[firstLevelSection][secondLevelSection][serviceKey] = serviceValue
+          } else if (sections[firstLevelSection]) {
+            sections[firstLevelSection][serviceKey] = serviceValue
+          }
+          // console.log(serviceKey);
+          // console.log(serviceValue);  
+          // console.log({
+          //   "key": serviceKey,
+          //   "value": serviceValue,
+          //   "line": line,
+          //   "firstLevelSection": firstLevelSection, 
+          //   "secondLevelSection": secondLevelSection, 
+          //   "thirdLevelSection": thirdLevelSection, 
+          //   "fourthLevelSection": fourthLevelSection
+          // })
+          // console.log(sections)
         }
       }
       resolve(sections);
