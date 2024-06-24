fcf08031968a40ff04eee7a2a21467cae3bcf7c8
Balou9 <emmanuel.owosekun@gmail.com>
2024-06-24 03:40:40 +0200

    fix: returns json accordingly

--- docs/index.js
+++ docs/index.js
@@ -8,29 +8,29 @@ function createFCSObjFromDocs(filePath) {
         return;
       }
 
-      const sections = {};
-      let firstLevelSection = '';
-      let secondLevelSection = '';
-      let thirdLevelSection = '';
-      let fourthLevelSection = '';
+      const sections = [];
+      // let firstLevelSection = '';
+      // let secondLevelSection = '';
+      // let thirdLevelSection = '';
+      // let fourthLevelSection = '';
 
       for (const line of data.split('\n')) {
-        if (line.startsWith('# ')) {
-          firstLevelSection = line.toLowerCase().trim().replace(/^(.){2}/,'').replace(/\W/g, '_');
-          sections[firstLevelSection] = sections[firstLevelSection] || {}; 
-        } 
-        if (line.startsWith('## ')) {
-          secondLevelSection = line.toLowerCase().trim().replace(/^(.){3}/,'').replace(/\W/g, '_')
-          sections[firstLevelSection][secondLevelSection] = sections[firstLevelSection][secondLevelSection] || {};
-        } 
-        if (line.startsWith('### ')) {
-          thirdLevelSection = line.toLowerCase().trim().replace(/^(.){4}/,'').replace(/\W/g, '_')
-          sections[firstLevelSection][secondLevelSection][thirdLevelSection] = sections[firstLevelSection][secondLevelSection][thirdLevelSection] || {};
-        } 
-        if (line.startsWith('#### ')) {
-          fourthLevelSection = line.toLowerCase().trim().replace(/^(.){5}/,'').replace(/\W/g, '_')
-           sections[firstLevelSection][secondLevelSection][thirdLevelSection][fourthLevelSection] = sections[firstLevelSection][secondLevelSection][thirdLevelSection][fourthLevelSection] || {};
-        } 
+        // if (line.startsWith('# ')) {
+        //   firstLevelSection = line.toLowerCase().trim().replace(/^(.){2}/,'').replace(/\W/g, '_');
+        //   sections[firstLevelSection] = sections[firstLevelSection] || {}; 
+        // } 
+        // if (line.startsWith('## ')) {
+        //   secondLevelSection = line.toLowerCase().trim().replace(/^(.){3}/,'').replace(/\W/g, '_')
+        //   sections[firstLevelSection][secondLevelSection] = sections[firstLevelSection][secondLevelSection] || {};
+        // } 
+        // if (line.startsWith('### ')) {
+        //   thirdLevelSection = line.toLowerCase().trim().replace(/^(.){4}/,'').replace(/\W/g, '_')
+        //   sections[firstLevelSection][secondLevelSection][thirdLevelSection] = sections[firstLevelSection][secondLevelSection][thirdLevelSection] || {};
+        // } 
+        // if (line.startsWith('#### ')) {
+        //   fourthLevelSection = line.toLowerCase().trim().replace(/^(.){5}/,'').replace(/\W/g, '_')
+        //    sections[firstLevelSection][secondLevelSection][thirdLevelSection][fourthLevelSection] = sections[firstLevelSection][secondLevelSection][thirdLevelSection][fourthLevelSection] || {};
+        // } 
         if (line.startsWith('[')) {
           let closingBracketIndex = line.indexOf(']');
           let preKey = line.substring(0, closingBracketIndex + 1);
@@ -39,20 +39,11 @@ function createFCSObjFromDocs(filePath) {
             if (keyMatch) {
               let serviceKey = keyMatch[1];
               let serviceValue = line.substring(closingBracketIndex + 1); 
-              console.log(serviceKey, serviceValue)
-            if (sections[firstLevelSection]?.[secondLevelSection]?.[thirdLevelSection]?.[fourthLevelSection]) {
-              sections[firstLevelSection][secondLevelSection][thirdLevelSection][fourthLevelSection][serviceKey] = serviceValue
-            } else if (sections[firstLevelSection]?.[secondLevelSection]?.[thirdLevelSection]) {
-              sections[firstLevelSection][secondLevelSection][thirdLevelSection][serviceKey] = serviceValue
-            } else if (sections[firstLevelSection]?.[secondLevelSection]) {
-              sections[firstLevelSection][secondLevelSection][serviceKey] = serviceValue
-            } else if (sections[firstLevelSection]) {
-              sections[firstLevelSection][serviceKey] = serviceValue
-            }
+              sections.push({"service": serviceKey, "description": serviceValue})
           }
         }
       }
-      resolve(sections);
+      resolve(JSON.stringify(sections));
     });
   });
 }
